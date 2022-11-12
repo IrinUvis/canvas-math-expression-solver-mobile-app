d8ddd6fa488d578681b594f7cbe158605ee951d5
Lidia <230459@edu.p.lodz.pl>
2022-11-12 16:03:12 +0100

    about screen

--- lib/canvas/views/about_screen.dart
+++ lib/canvas/views/about_screen.dart
@@ -1,5 +1,6 @@
 import 'package:canvas_equation_solver_mobile_app/canvas/widgets/app_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:canvas_equation_solver_mobile_app/theme/colors.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({Key? key}) : super(key: key);
@@ -7,8 +8,46 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(
+                Icons.menu,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
       drawer: const AppDrawer(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            Text(
+              'Canvas Equation Solver',
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    color: orange200,
+                  ),
+            ),
+            const SizedBox(height: 30),
+            const CircleAvatar(
+              radius: 80,
+              backgroundImage: AssetImage('assets/icon.jpg'),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              'Draw a simple equation\nand the app will solve it for you!',
+              style: Theme.of(context).textTheme.headline5,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
